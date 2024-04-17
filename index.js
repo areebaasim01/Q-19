a013b5653fe846cf06194f2408037c0f5113615b
areebaasim01 <161856617+areebaasim01@users.noreply.github.com>
2024-04-17 17:54:49 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+// 19.  Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating
+// the number of people you are inviting to dinner.
+Object.defineProperty(exports, "__esModule", { value: true });
+let guestArr = ["Yusra", "Ardil", "Imran", "Saima", "Areeba"];
+let numberOfGuests = guestArr.length;
+console.log(`I am inviting ${numberOfGuests} people to dinner.`);
